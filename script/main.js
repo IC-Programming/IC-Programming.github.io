641dee74d0c3e1d809d8fc2239233181b25e2749
IC-Programming <ic.imesh.chamara@gmail.com>
2019-04-17 09:04:32 +0530

    Add the Copyright to the files

--- script/main.js
+++ script/main.js
@@ -1,1 +1,2 @@
+/* Copyright © Imesh Chamara 2019 */
 function topMenu(){var scr=document.getElementById("ic-top-drbtn");if(scr.style.display!="block") scr.style.display="block";else scr.style.display="none";}
